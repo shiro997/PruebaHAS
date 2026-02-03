d3fccaac52185b164347b4ca0ed5ffe22a3550ed
Mateo Blanco <mateo997@outlook.com>
2026-02-03 14:17:13 -0500

    archivo de script de base de datos, con creación de la base de datos y las tablas

---
+++ Scripts/Database-Creation.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE PruebaHas;
+
+use PruebaHas go;
+
+--creación de tablas
+create table Usuario(IdUsuario int primary key, NombreUsuario nvarchar(max), Email nvarchar(max), usrPassword nvarchar(max))
+
+create table Cliente (IdCliente int primary key, NombreCliete nvarchar(max))
+
+create table Categoria (IdCategoria int primary key, NombreCategoria nvarchar(max))
+
+create table Producto (IdProducto int primary key, NombreProducto nvarchar(max), IdCategoria int, PrecioProducto int, IsAgotado bit)
+
+create table Venta (IdVenta int primary key, IdUsuario int, IdCliente int, TotalVenta int)
+
+create table VentaProducto (IdVenta int, IdProducto int, CantidadProducto int)
+
