21dc26c3d7ab163524edb494cf7fc583599862cc
Mateo Blanco <mateo997@outlook.com>
2026-02-05 08:29:01 -0500

    limpieza de entidades
     se retiran las entidades no utilizadas

--- Scripts/Database-Creation.sql
+++ Scripts/Database-Creation.sql
@@ -1,17 +1,7 @@
 CREATE DATABASE PruebaHas;
 
-use PruebaHas go;
+use PruebaHas
 
---creación de tablas
+--creación de tabla
 create table Usuario(IdUsuario int identity(1,1) primary key, NombreUsuario nvarchar(max), Email nvarchar(max), UsrPassword nvarchar(max))
-
-create table Cliente (IdCliente int primary key, NombreCliete nvarchar(max))
-
-create table Categoria (IdCategoria int primary key, NombreCategoria nvarchar(max))
-
-create table Producto (IdProducto int primary key, NombreProducto nvarchar(max), IdCategoria int, PrecioProducto int, IsAgotado bit)
-
-create table Venta (IdVenta int primary key, IdUsuario int, IdCliente int, TotalVenta int)
-
-create table VentaProducto (IdVenta int, IdProducto int, CantidadProducto int)
 
